5ad9bd869157c4916d517f933625e28497d8d75b
eliaszoo <wang.z.q.ace@gmail.com>
2024-04-07 23:18:33 +0800

    最长子序列

--- code/golang/tp/coins.go
+++ code/golang/tp/coins.go
@@ -37,7 +37,35 @@ func CoinChange(coins []int, amount int) int {
 
 }
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func lengthOfLIS(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	tp := make([]int, len(nums)+1)
+	tp[0] = 0
+	tp[1] = 1
+	for k := 2; k <= len(nums); k++ {
+		for i := 0; i < k-1; i++ {
+			if nums[k-1] > nums[i] {
+				tp[k] = max(tp[k], tp[i+1]+1)
+			}
+		}
+	}
+	return tp[len(nums)]
+}
+
 func main() {
-	val := CoinChange([]int{1, 2, 5}, 11)
-	fmt.Println(val)
+	//val := CoinChange([]int{1, 2, 5}, 11)
+	//fmt.Println(val)
+
+	fmt.Println(lengthOfLIS([]int{4, 10, 4, 3, 8, 9}))
 }
