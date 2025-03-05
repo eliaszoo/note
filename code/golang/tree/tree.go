8a816fc8147e6abe9c447c611ff38c7ce9a1246e
elias <eliaswang@zego.im>
2025-03-05 19:35:30 +0800

    tree

--- code/golang/tree/tree.go
+++ code/golang/tree/tree.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type TreeNode struct {
 	Val   int
@@ -175,6 +178,50 @@ func swap(root *TreeNode) {
 	swap(root.Right)
 }
 
+func symmetry(left *TreeNode, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	} else if (left != nil && right == nil) || (left == nil && right != nil) {
+		return false
+	} else {
+		if left.Val != right.Val {
+			return false
+		}
+	}
+
+	outside := symmetry(left.Left, right.Right)
+	inside := symmetry(left.Right, right.Left)
+	return outside && inside
+}
+
+func height(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	l := height(root.Left) + 1
+	r := height(root.Right) + 1
+	return int(math.Max(float64(l), float64(r)))
+}
+
+func MinHeight(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return minHeight(root)
+}
+
+func minHeight(root *TreeNode) int {
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+
+	l := height(root.Left) + 1
+	r := height(root.Right) + 1
+	return int(math.Min(float64(l), float64(r)))
+}
+
 func main() {
 	root := build([]int{1, 2, 3, 4, 5, 6, 7}, 0)
 	var tmp []int
@@ -204,4 +251,14 @@ func main() {
 	swap(root)
 	fmt.Println(traverG(root))
 	fmt.Println(traverG2(root))
+
+	root2 := build([]int{1, 2, 2, 3, 4, 4, 3, 5}, 0)
+	fmt.Println(traverG2(root2))
+	fmt.Println(symmetry(root2.Left, root2.Right))
+
+	fmt.Println(height(root))
+	fmt.Println(height(root2))
+
+	root3 := build([]int{1, 2, 3, 4}, 0)
+	fmt.Println(MinHeight(root3))
 }
